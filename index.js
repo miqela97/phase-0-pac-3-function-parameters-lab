7bbc35de5d2f19378a1040c86c55705ea4ea716f
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-07-12 02:51:30 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-function shout(string) {
-    return string.toUpperCase();
+function introduction(name){
+    return `Hi, my name is ${name}.`
 }
-function whisper(string) {
-  return string.toLowerCase();
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-function logShout(string) {
-  console.log(string.toUpperCase());
+function introductionWithLanguageOptional(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
 }
-function logWhisper(string) {
-  console.log(string.toLowerCase());
-}
-function sayHiToHeadphonedRoommate(string) {
-  if (string === string.toLowerCase()) {
-    return "I can't hear you!";
-  } else if (string === string.toUpperCase()) {
-    return "YES INDEED!";
-  } else if (string === "Let's have dinner together!") {
-    return "I would love to!";
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
-}
